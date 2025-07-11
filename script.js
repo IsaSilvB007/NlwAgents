e3563aa43d36beca9ae6a96d5d64a697fc67ac42
Isadora Bispo <158217976+IsaSilvB007@users.noreply.github.com>
2025-07-11 20:35:59 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -10,8 +10,6 @@ const markdownToHTML = (text) => {
     return converter.makeHtml(text)
 }
 
-
-// AIzaSyCTw5a1mBdlPMAkExMovDTBaYPcb0vrvug
 const perguntarAI = async (question, game, apiKey) => {
     const model = "gemini-2.5-flash"
     const geminiURL = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${apiKey}`
